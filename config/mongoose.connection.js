88fcac118980ce6b648a14e7770b36b7b9e7cf0f
Nilesh Kaute <nileshkaute1@gmail.com>
2025-11-16 15:17:29 +0530

    Deleted everything

--- config/mongoose.connection.js
+++
@@ -1,7 +0,0 @@
-// Connect to MongoDB
-const mongoose=require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/scatch')
-  .then(() => console.log('✅ MongoDB connected'))
-  .catch(err => console.log('❌ Connection error:', err));
-
-  module.exports=mongoose.connection;
